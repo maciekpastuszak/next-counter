ea94aeca7166f76fcbbf7186e6be9284daaeb6d0
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2026-01-03 10:54:19 +0100

    Add Counter component and update main page layout

---
+++ app/counter/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <h1>Licznik</h1>
+      <p>To jest strona licznika.</p>
+    </div>
+  );
+};
+
+export default Counter;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div>
+      <h1>Licznik do nauki</h1>
+      <Link href="/counter">Przejdź do licznika</Link>
+    </div>
+  );
 }
